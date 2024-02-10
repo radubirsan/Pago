be4ad8451bb90635a08a309e8d51649a2b804db5
BIRSAN RADU IOSIF <radubirsan>
2024-02-10 15:19:05 +0200

    Add local storage and save the JSON

--- Pago/Model/Model.swift
+++ Pago/Model/Model.swift
@@ -25,13 +25,73 @@ import SwiftUI
     var httpClient: NetworkCallManager = NetworkCallManager.shared
     static let shared = Model()
     
+    let container:NSPersistentContainer
+    @Published var savedEntities:[ContactEntity] = []
+    
     private init() {
         self.httpClient = NetworkCallManager.shared
+        container = NSPersistentContainer(name:"ContactsContainer")
+        container.loadPersistentStores { (description, error ) in
+            if let error = error {
+                print ("ERROR LOADING CODE DATA \(error)")
+            }
+            else {
+                
+                print("🟩")
+            }
+        }
     }
 
     func getContacts() {
-        Task {
-            contacts = try await NetworkCallManager.shared.getContactsFromURL()
+        //only grab the JSON if there are 0 contacts stored in core data
+        if(savedEntities.count == 0) {
+            Task {
+                do {
+                    contacts = try await NetworkCallManager.shared.getContactsFromURL()
+                    saveContactsToCoreData(contacts)
+                } catch {
+                    fatalError("Cannot get contacts")
+                }
+            }
+        }
+        else{
+            print("🟪",savedEntities.count)
+        }
+    }
+    
+    
+    func saveContactsToCoreData(_ contacts: [Contact]) {
+           
+            for contact in contacts {
+                
+                let contactEntity = ContactEntity(context: container.viewContext)
+                contactEntity.id = Int32(contact.id)
+                contactEntity.name = contact.name
+                contactEntity.email = contact.email
+                contactEntity.gender = contact.gender
+                contactEntity.status = contact.status
+                saveData()
+                print("Save 🟨" ,contactEntity.name ?? "no name", contactEntity.id)
+            }
+        }
+    
+    func saveData(){
+        do {
+            try  container.viewContext.save()
+            fetchContact()
+        }
+        catch let error {
+            print ("Error saving \(error)" )
+        }
+    }
+    
+    func fetchContact(){
+        
+        let request = NSFetchRequest<ContactEntity>(entityName: "ContactEntity")
+        do{
+           savedEntities  =  try container.viewContext.fetch(request)
+        }catch let error {
+            print ("Error fetching \(error)")
         }
     }
 
