0a71e7c43e09f50e612197224abe95eb3d790056
BIRSAN RADU IOSIF <radubirsan>
2024-02-10 13:54:00 +0200

    Initial Commit

---
+++ Pago/PagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagoApp.swift
+//  Pago
+//
+//  Created by radu on 10.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
